f355e7aa88c2e5e9db38fc93d11a2c89e3f4bad5
Aman Mangal <mangalaman93@gmail.com>
2026-07-08 01:15:10 +0530

    revamp the website for 60 years' celebrations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,19 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        umang: {
+          purple: '#b579d2',
+          cyan: '#33ccbf',
+          green: '#7dd24a',
+        },
+      },
+      fontFamily: {
+        sans: ['"Source Sans 3"', 'sans-serif'],
+        heading: ['Quicksand', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
